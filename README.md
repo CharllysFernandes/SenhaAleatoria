9455f04ae26f8e5be52f7facab16b4657805928a
Charllys Fernandes <charllys.daiany@gmail.com>
2025-03-19 23:22:00 -0300

    update code.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-http://charllysfernandes.github.io/SenhaAleatoria/
+# Senha Aleatória
+
+Este projeto é uma ferramenta para gerar senhas aleatórias seguras. Dividir a senha em blocos facilita a visualização das senhas e a memorização.
+
+## Para que serve
+
+A ferramenta serve para criar senhas fortes e seguras, que são difíceis de serem descobertas por terceiros.
+
+## Como usar
+
+1. Acesse a ferramenta através do link abaixo:
+   http://charllysfernandes.github.io/SenhaAleatoria/
+2. Gere uma senha aleatória.
+3. Divida a senha em blocos para facilitar a visualização e memorização.
+4. Lembre-se de que a senha é salva na memória do navegador e deve ser apagada após o uso para garantir a segurança.
+
+
--- src/script.js
+++ src/script.js
@@ -4,55 +4,74 @@ function gerarSenha() {
     const usarNumeros = document.getElementById("usarNumeros").checked;
     const usarCaracteresEspeciais = document.getElementById("usarCaracteresEspeciais").checked;
 
-    let chars = "";
-
-    if (usarLetras) {
-        chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    }
-    if (usarNumeros) {
-        chars += "0123456789";
-    }
-    if (usarCaracteresEspeciais) {
-        chars += "!@#$%^&*()_=+[]{}|;:'\",.<>?/`~";
-    }
-
+    const chars = obterCaracteres(usarLetras, usarNumeros, usarCaracteresEspeciais);
     const tamanhoSenha = document.getElementById("tamanhoSenha").value;
-    let senha = "";
-    for (let i = 0; i < tamanhoSenha; i++) {
-        senha += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
+    const senha = gerarStringAleatoria(chars, tamanhoSenha);
 
-    // Divisão da senha em blocos conforme o tamanho
-    let senhaFormatada = formatarSenha(tamanhoSenha, senha);
-
+    const senhaFormatada = formatarSenha(tamanhoSenha, senha);
     document.getElementById("senha").value = senhaFormatada.trim();
 
-    // Calcular o tempo para quebrar a senha
     calcularTempoQuebra(chars.length, tamanhoSenha);
-
-    // Salvar a senha no Local Storage
     salvarSenhaNoStorage(senhaFormatada.trim());
-
-    // Atualizar o histórico
     carregarSenhasDoStorage();
+}
+
+// Função para obter os caracteres com base nas opções selecionadas
+function obterCaracteres(usarLetras, usarNumeros, usarCaracteresEspeciais) {
+    let chars = "";
+    if (usarLetras) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (usarNumeros) chars += "0123456789";
+    if (usarCaracteresEspeciais) chars += "!@#$%^&*()_=+[]{}|;:'\",.<>?/`~";
+    return chars;
+}
+
+// Função para gerar uma string aleatória
+function gerarStringAleatoria(chars, tamanho) {
+    let resultado = "";
+    for (let i = 0; i < tamanho; i++) {
+        resultado += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return resultado;
 }
 
 // Função para formatar a senha com base no tamanho
 function formatarSenha(tamanhoSenha, senha) {
     let senhaFormatada = "";
     if (tamanhoSenha > 16) {
-        for (let i = 0; i < senha.length; i += 8) {
-            senhaFormatada += senha.slice(i, i + 8) + "-";
-        }
+        senhaFormatada = dividirEmBlocos(senha, 8);
     } else if (tamanhoSenha > 12) {
-        for (let i = 0; i < senha.length; i += 6) {
-            senhaFormatada += senha.slice(i, i + 6) + "-";
-        }
+        senhaFormatada = dividirEmBlocos(senha, 6);
     } else {
-        let metade = Math.ceil(senha.length / 2);
-        senhaFormatada = senha.slice(0, metade) + "-" + senha.slice(metade);
+        senhaFormatada = dividirAoMeio(senha);
     }
     return senhaFormatada;
+}
+
+// Função para dividir a senha em blocos
+function dividirEmBlocos(senha, tamanhoBloco) {
+    let resultado = "";
+    const numBlocos = Math.floor(senha.length / tamanhoBloco);
+    const tamanhoFinalBloco = Math.ceil(senha.length / numBlocos);
+
+    if (senha.length > 12) {
+        for (let i = 3; i <= tamanhoFinalBloco; i++) {
+            if (senha.length % i === 0 && i !== 2) {
+                tamanhoBloco = i;
+                break;
+            }
+        }
+    }
+
+    for (let i = 0; i < senha.length; i += tamanhoBloco) {
+        resultado += senha.slice(i, i + tamanhoBloco) + "-";
+    }
+    return resultado.slice(0, -1); // Remove o último hífen
+}
+
+// Função para dividir a senha ao meio
+function dividirAoMeio(senha) {
+    const metade = Math.ceil(senha.length / 2);
+    return senha.slice(0, metade) + "-" + senha.slice(metade);
 }
 
 // Função para copiar a senha
@@ -61,8 +80,6 @@ function copiarSenha() {
     senhaInput.select();
     senhaInput.setSelectionRange(0, 99999); // Para dispositivos móveis
     document.execCommand("copy");
-
-    // Exibe uma mensagem de confirmação
     alert("Senha copiada para a área de transferência!");
 }
 
@@ -78,32 +95,28 @@ function calcularTempoQuebra(numCaracteres, comprimentoSenha) {
     const totalCombinacoes = Math.pow(numCaracteres, comprimentoSenha);
     const tempoSegundos = totalCombinacoes / tentativasPorSegundo;
     const tempoDias = tempoSegundos / (60 * 60 * 24);
-    let tempoQuebra = "";
-
-    if (tempoDias > 365 * 10000000000) {
-        const tempoMilhoes = tempoDias / (365 * 1000000000);
-        tempoQuebra = `Tempo estimado para quebrar a senha: aproximadamente ${tempoMilhoes.toExponential(2)} milhões de anos.`;
-    } else if (tempoDias > 365 * 1000000) {
-        const tempoMilenios = tempoDias / (365 * 1000);
-        tempoQuebra = `Tempo estimado para quebrar a senha: aproximadamente ${tempoMilenios.toFixed(0)} milênios.`;
-    } else if (tempoDias > 365 * 100) {
-        const tempoSeculos = tempoDias / (365 * 100);
-        tempoQuebra = `Tempo estimado para quebrar a senha: aproximadamente ${tempoSeculos.toFixed(0)} séculos.`;
-    } else if (tempoDias > 365 * 10) {
-        const tempoDecadas = tempoDias / (365 * 10);
-        tempoQuebra = `Tempo estimado para quebrar a senha: aproximadamente ${tempoDecadas.toFixed(0)} décadas.`;
-    } else if (tempoDias > 365) {
-        const tempoAnos = tempoDias / 365;
-        tempoQuebra = `Tempo estimado para quebrar a senha: aproximadamente ${tempoAnos.toFixed(0)} anos.`;
-    } else {
-        tempoQuebra = `Tempo estimado para quebrar a senha: aproximadamente ${tempoDias.toFixed(0)} dias.`;
-    }
+    const tempoQuebra = calcularTempoQuebraTexto(tempoDias);
 
     const tempoQuebraElement = document.getElementById("tempoQuebra");
     tempoQuebraElement.innerText = tempoQuebra;
-
-    // Cria o ícone de tooltip
     criarTooltip(tempoQuebraElement);
+}
+
+// Função para calcular o texto do tempo de quebra
+function calcularTempoQuebraTexto(tempoDias) {
+    if (tempoDias > 365 * 10000000000) {
+        return `Tempo estimado para quebrar a senha: aproximadamente ${(tempoDias / (365 * 1000000000)).toExponential(2)} milhões de anos.`;
+    } else if (tempoDias > 365 * 1000000) {
+        return `Tempo estimado para quebrar a senha: aproximadamente ${(tempoDias / (365 * 1000)).toFixed(0)} milênios.`;
+    } else if (tempoDias > 365 * 100) {
+        return `Tempo estimado para quebrar a senha: aproximadamente ${(tempoDias / (365 * 100)).toFixed(0)} séculos.`;
+    } else if (tempoDias > 365 * 10) {
+        return `Tempo estimado para quebrar a senha: aproximadamente ${(tempoDias / (365 * 10)).toFixed(0)} décadas.`;
+    } else if (tempoDias > 365) {
+        return `Tempo estimado para quebrar a senha: aproximadamente ${(tempoDias / 365).toFixed(0)} anos.`;
+    } else {
+        return `Tempo estimado para quebrar a senha: aproximadamente ${tempoDias.toFixed(0)} dias.`;
+    }
 }
 
 // Função para criar tooltip
@@ -136,13 +149,11 @@ function carregarSenhasDoStorage() {
     historicoSenhas.innerHTML = ""; // Limpar o conteúdo anterior
 
     let senhas = JSON.parse(localStorage.getItem("senhas")) || [];
-
     senhas.forEach((senha, index) => {
         const li = document.createElement("li");
         li.className = "fw-lighter text-light list-group-item list-group-item-action d-flex justify-content-between align-items-center bg-dark";
         li.innerText = senha;
 
-        // Botão de excluir
         const btnExcluir = document.createElement("span");
         btnExcluir.className = "text-danger bi bi-x-lg text-light";
         btnExcluir.onclick = function () {
